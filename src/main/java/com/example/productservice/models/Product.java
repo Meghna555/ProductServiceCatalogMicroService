04f9ff42cedf328a4bafb7d93f987c18afe804a8
Meghna555 <167201393+Meghna555@users.noreply.github.com>
2026-05-12 15:51:16 +0530

    Intro to Spring JPA, JDBC, ORM

--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,16 +1,27 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity(name = "products")
 public class Product extends BaseModel{
     private String title;
     private String description;
     private Double price;
     private String imageUrl;
     private int qty;
+
+    @ManyToOne
     private Category category;
+
+    /*Product <-> Category => M : 1
+        here, 1 P -> 1 C
+               1 C ->  Many P
+     */
 
 }
